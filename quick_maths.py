8f34a2cfc384234521a44ead363efa5985a40fc3
joissa <itsjoshissa@gmail.com>
2022-02-24 13:38:14 -0500

    documentation
    Added documentation to the vector magnitude method.

--- quick_maths.py
+++ quick_maths.py
@@ -1,4 +1,12 @@
 import numpy as np
 
 def vector_magnitude(vec1):
+    """Calculates the magnitude of a vector.
+    INPUTS:
+        vec1 - The vector. Expected to be a 1D numpy array.
+
+    OUTPUTS:
+        Outputs the magnitude of the input vector.
+    """
+
     return np.sqrt(np.sum(np.power(vec1, 2)))
