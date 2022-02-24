6b493554a5a80b927a7d5b223cc341107d0baab8
joissa <itsjoshissa@gmail.com>
2022-02-24 13:36:02 -0500

    update

--- gravity.py
+++ gravity.py
@@ -5,19 +5,25 @@ class Gravity:
     def __init__(self, obj1, obj2):
         """Initializes the gravity engine between two objects.
         INPUTS:
-            obj1, obj2 - The two objects that are orbitting each other.
+            obj1, obj2 - The two objects that are orbiting each other.
         """
 
         self.obj1 = obj1
         self.obj2 = obj2
 
-    def orbit():
-        """Performs the gravitational movements.
+    def force_gravity():
+        """Calculates the force of gravity between two objects.
+        INPUTS:
+            obj1, obj2 - The two objects that are interacting.
         OUTPUTS:
-            Updated positions, velocities, and accelerations for obj1, obj2.
+
         """
 
-        #The Newtonian model for gravity is given to us as
-        # F = Gm1m2/r21
+        #The Newtonian model for gravity is given to us as F = Gm1m2/r21^2
+        #To set this as a 3D vector, Fvec = Gm1m2/||r21||^3 * rvec
+        
+        r21 = obj2.position - obj1.position
+
+
 
         pass
---
+++ quick_maths.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def vector_magnitude(vec1):
+    return np.sqrt(np.sum(np.power(vec1, 2)))
