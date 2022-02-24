ee8ac426948eefc7367cf4f278cfd8034bd2eac6
joissa <itsjoshissa@gmail.com>
2022-02-24 13:56:20 -0500

    Update
    Tests if you can get forces.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,27 @@
+import numpy as np
+
+from Massive_Object import Massive_Object
+from gravity import Gravity
+
 def main():
-    pass
+
+    Msun = 1.99e30
+    Rsun = 696e6
+    AU = 1.496e11
+
+    #sun and earth, starting at rest
+    m1, m2 = Msun, Msun/1.3e6
+    r1, r2 = Rsun, 0.009*Rsun
+    p1, p2 = np.array([0, 0, 0]), np.array([AU/np.sqrt(3), AU/np.sqrt(3), AU/np.sqrt(3)])
+    v1, v2 = np.array([0, 0, 0]), np.array([0, 0, 0])
+    a1, a2 = np.array([0, 0, 0]), np.array([0.006, 0.006, 0])
+
+    obj1 = Massive_Object(m1, r1, p1, v1, a1) #the Sun
+    obj2 = Massive_Object(m2, r2, p2, v2, a2) #the Earth
+
+    F21, F12 = Gravity(obj1, obj2).force_gravity()
+
+    print(F12, F21)
 
 if __name__ == '__main__':
     main()
--- gravity.py
+++ gravity.py
@@ -13,7 +13,7 @@ class Gravity:
         self.obj1 = obj1
         self.obj2 = obj2
 
-    def force_gravity():
+    def force_gravity(self):
         """Calculates the force of gravity between two objects.
         INPUTS:
             obj1, obj2 - The two objects that are interacting.
@@ -27,10 +27,10 @@ class Gravity:
         #The Newtonian model for gravity is given to us as F = Gm1m2/r21^2.
         #To set this as a 3D vector, Fvec = Gm1m2/||r21||^3 * rvec.
 
-        r21_vec = obj2.position - obj1.position
+        r21_vec = self.obj2.position - self.obj1.position
         r21_mag = vector_magnitude(r21_vec)
 
-        F21 = G*obj1.mass*obj2.mass/(r21_mag**3) * r21_vec
+        F21 = G*self.obj1.mass*self.obj2.mass/(r21_mag**3) * r21_vec
         F12 = -F21
 
         return F21, F12
