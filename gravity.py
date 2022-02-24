8075dcfa9cdfe371c4d166ba84a8dee876394f38
joissa <itsjoshissa@gmail.com>
2022-02-24 13:26:39 -0500

    Create gravity.py

---
+++ gravity.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Gravity:
+
+    def __init__(self, obj1, obj2):
+        """Initializes the gravity engine between two objects.
+        INPUTS:
+            obj1, obj2 - The two objects that are orbitting each other.
+        """
+
+        self.obj1 = obj1
+        self.obj2 = obj2
+
+    def orbit():
+        """Performs the gravitational movements.
+        OUTPUTS:
+            Updated positions, velocities, and accelerations for obj1, obj2.
+        """
+
+        #The Newtonian model for gravity is given to us as
+        # F = Gm1m2/r21
+
+        pass
